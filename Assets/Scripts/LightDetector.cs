68e94c5f6db05f6a1fc21bfafa865a7a61289670
Farry1 <mail@lukasfabry.de>
2019-08-02 22:36:00 +0200

    Fairy Light Detection

---
+++ Assets/Scripts/LightDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetector : MonoBehaviour
+{
+    public bool isDetected;
+    LayerMask layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForLightHit();
+    }
+
+    void CheckForLightHit()
+    {
+        Vector3 direction = GhostController.Instance.transform.position - transform.position;
+        RaycastHit hit;
+
+        Ray ray = new Ray(transform.position, direction);
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "Ghost")
+            {
+                Debug.Log("hit");
+            }
+        }
+    }
+}
