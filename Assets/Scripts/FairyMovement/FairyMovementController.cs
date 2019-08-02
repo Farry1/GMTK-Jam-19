f2256452a033c3429de7ae2cfbbca26413c0eeee
Farry1 <mail@lukasfabry.de>
2019-08-03 01:24:50 +0200

    movement with line renderer preview and max move distance

---
+++ Assets/Scripts/FairyMovement/FairyMovementController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyMovementController : MonoBehaviour
+{
+    FairyMovement selectedFairyMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            if (selectedFairyMovement != null)
+            {
+                if (selectedFairyMovement.CalculatePath(hit.point) > selectedFairyMovement.pathLengthLeft)
+                {
+                    Debug.Log("Can't Move");
+                    selectedFairyMovement.canMove = false;
+                    selectedFairyMovement.lineRenderer.SetColors(Color.red, Color.red);
+                }
+                else
+                {
+                    Debug.Log("Can Move");
+                    selectedFairyMovement.canMove = true;
+                    selectedFairyMovement.lineRenderer.SetColors(Color.white, Color.white);
+                }
+            }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    if (selectedFairyMovement != null)
+                        selectedFairyMovement.isSelected = false;
+
+                    selectedFairyMovement = hit.collider.gameObject.GetComponent<FairyMovement>();
+                    selectedFairyMovement.isSelected = true;
+                }
+                else
+                {
+                    if (selectedFairyMovement != null && selectedFairyMovement.canMove)
+                    {
+                        selectedFairyMovement.SetPath();
+                        selectedFairyMovement = null;
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (selectedFairyMovement != null)
+                {
+                    selectedFairyMovement.isSelected = false;
+                    selectedFairyMovement = null;                    
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PathCalculations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathCalculations : MonoBehaviour
+{
+    public static float PathLength(NavMeshPath path)
+    {
+        if (path.corners.Length < 2)
+            return 0;
+
+        Vector3 previousCorner = path.corners[0];
+        float lengthSoFar = 0.0F;
+        int i = 1;
+        while (i < path.corners.Length)
+        {
+            Vector3 currentCorner = path.corners[i];
+            lengthSoFar += Vector3.Distance(previousCorner, currentCorner);
+            previousCorner = currentCorner;
+            i++;
+        }
+        return lengthSoFar;
+    }
+}
